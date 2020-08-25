49995926e735767af65ed3f9a9b0ae542a37ba91
matheus <mtava002@gmail.com>
2020-08-25 13:59:23 -0400

    changed js for typewriter effect, added title.

--- script.js
+++ script.js
@@ -2,23 +2,35 @@
 
 
 let clickheader = document.querySelectorAll('.dropdown h3');
-for (i=0; i< clickheader.length; i++) {
+for (i = 0; i < clickheader.length; i++) {
     let test1 = clickheader[i];
-   test1.onclick = togDisplay;
-    }
+    test1.onclick = togDisplay;
+}
 
 
 function togDisplay(e) {
     console.log(e.target.parentElement.querySelector('.skills'), this)
     e.target.parentElement.querySelector('.skills').classList.toggle('display')
+
+
 }
-
+let typing = false
+document.querySelector('#qualifications').addEventListener('click', function () {
+    console.log('test', !typing);
+    if (!typing){
+        typeWriter();
+        typing = true  
+    } 
+})
 let txt = 'Junior Cloud Engineer with a focus on infrastructure, system architecture and operations. A strong interest in networking, web technologies, cybersecurity, process innovation, and business management. Nearly a decade of experience maintaining information systems including engagements at multinational corporations and small businesses with a focus on cloud architecture, system administration, technical writing, project management, and business administration. Responsible, self-directed, and able to quickly learn and apply complex concepts and processes.'
-let speed = 50;
+let speed = 8;
 
+
+let letter = 0
 function typeWriter() {
-    for (i=0; i< txt.length; i++) {
-            document.querySelector('.dropdown p').innerHTML += txt.charAt(i);
-            setTimeout(typeWriter, speed);
-        }
+    if (letter < txt.length) {
+        document.querySelector('.dropdown p').innerHTML += txt.charAt(letter);
+        letter++;
+        setTimeout(typeWriter, speed);
     }
+}
