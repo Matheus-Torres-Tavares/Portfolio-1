2f4468f49b95bc8e62fa1d949a87b9f960b4ee05
matheus <mtava002@gmail.com>
2020-08-26 19:11:12 -0400

    added API integration via the youtube API for random classical music videos

--- script.js
+++ script.js
@@ -17,10 +17,10 @@ function togDisplay(e) {
 let typing = false
 document.querySelector('#qualifications').addEventListener('click', function () {
     console.log('test', !typing);
-    if (!typing){
+    if (!typing) {
         typeWriter();
-        typing = true  
-    } 
+        typing = true
+    }
 })
 let txt = 'Junior Cloud Engineer with a focus on infrastructure, system architecture and operations. A strong interest in networking, web technologies, cybersecurity, process innovation, and business management. Nearly a decade of experience maintaining information systems including engagements at multinational corporations and small businesses with a focus on cloud architecture, system administration, technical writing, project management, and business administration. Responsible, self-directed, and able to quickly learn and apply complex concepts and processes.'
 let speed = 3;
@@ -34,3 +34,21 @@ function typeWriter() {
         setTimeout(typeWriter, speed);
     }
 }
+
+document.querySelector('#clickButton').onclick = getrandomVid;
+function getrandomVid() {
+    axios.get('https://www.googleapis.com/youtube/v3/search?q=violin%20concerto&part=snippet&key=AIzaSyAFhCTNcd8nEJtkP7rrWwOJI8l5SxpiDjI&maxResults=1000&pageToken')
+        .then(res => {
+            console.log(res.data)
+            showVideo(res.data.items)
+        })
+}
+
+
+function showVideo(items) {
+    let randomVideo = items[Math.floor(Math.random() * items.length)]
+    console.log(randomVideo.id.videoId)
+
+
+    document.querySelector('#videoLocation').innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${randomVideo.id.videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+} 
