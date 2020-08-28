b33ba0478708d3202af9ac910a12c76eca54029c
matheus <mtava002@gmail.com>
2020-08-27 20:21:35 -0400

    API integration and contact form

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.querySelector('#qualifications').addEventListener('click', function () 
     }
 })
 let txt = 'Junior Cloud Engineer with a focus on infrastructure, system architecture and operations. A strong interest in networking, web technologies, cybersecurity, process innovation, and business management. Nearly a decade of experience maintaining information systems including engagements at multinational corporations and small businesses with a focus on cloud architecture, system administration, technical writing, project management, and business administration. Responsible, self-directed, and able to quickly learn and apply complex concepts and processes.'
-let speed = 3;
+let speed = 5;
 
 
 let letter = 0
@@ -35,9 +35,9 @@ function typeWriter() {
     }
 }
 
-document.querySelector('#clickButton').onclick = getrandomVid;
+document.querySelector('.clickButton').onclick = getrandomVid;
 function getrandomVid() {
-    axios.get('https://www.googleapis.com/youtube/v3/search?q=violin%20concerto&part=snippet&key=AIzaSyAFhCTNcd8nEJtkP7rrWwOJI8l5SxpiDjI&maxResults=1000&pageToken')
+    axios.get('https://www.googleapis.com/youtube/v3/search?q=violin%20concerto&part=snippet&key=AIzaSyCs2dP_8PZVBWQvktrnrZAWxNxApjAfTxg&maxResults=1000')
         .then(res => {
             console.log(res.data)
             showVideo(res.data.items)
@@ -53,10 +53,26 @@ function showVideo(items) {
     document.querySelector('#videoLocation').innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${randomVideo.id.videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
 }
 
+document.querySelector('#ironclass').onclick = populateIron;
 
-axios
-    .get(`https://ironrest.herokuapp.com/mia-aug2020-webdevs`)
-    .then((res) => {
-        for (let i of res.data)
-            console.log(i.first_name)
-    })
+let ironhackTyping = false
+
+function populateIron() {
+    if (!ironhackTyping) {
+
+
+        axios
+            .get(`https://ironrest.herokuapp.com/mia-aug2020-webdevs`)
+            .then((res) => {
+                let ironhacker = res.data;
+                ironhackTyping = true;
+                for (i = 0; i < ironhacker.length; i++) {
+                    let list = document.querySelector('#linkFormat2')
+                    console.log(list);
+                    list.innerHTML += `<li><a href=${ironhacker[i].website}>${ironhacker[i].first_name} ${ironhacker[i].last_name}</a></li>`
+                }
+
+            })
+    }
+
+}
